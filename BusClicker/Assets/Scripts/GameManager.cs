25f167a2144fe773245c5d3f9c5863c222797692
AIE\s229025 <jrverburg@yahoo.com>
2023-08-31 12:41:21 -0700

    scripts

--- BusClicker/Assets/Scripts/GameManager.cs
+++ BusClicker/Assets/Scripts/GameManager.cs
@@ -6,6 +6,21 @@ public class GameManager : MonoBehaviour
 {
     public float BloodMoney;
     public int Prestige;
+    public GameObject Victim;
+    public GameObject level;
+    public int BusCount;
+    public bool Spawning;
+
+    public void Update()
+    {
+        if(Spawning == false)
+        {
+            SpawnVictim(BusCount);
+        }
+    }
+
+
+
 
     void AddValue(int Add, int Multiplier)
     {
@@ -17,10 +32,23 @@ public class GameManager : MonoBehaviour
     }
     void SpawnVictim(int Amount)
     {
-        var Spawned = new int 0
-        while(Spawned |= Amount)
+        Spawning = true;
+        int Spawned = 0;
+        while(true)
         {
-          
-        }
+            Instantiate(Victim, SpawnLocation(level), Quaternion.identity, null);
+            if (Spawned == Amount)
+            {
+                break;
+
+            }
+    }
+        Spawning = false;
+    }
+    Vector3 SpawnLocation(GameObject Map)
+    {
+
+     var location = new Vector3( Random.Range(-8f,8f), Random.Range(-3.7f,3.2f), 0);
+      return location;
     }
 }
